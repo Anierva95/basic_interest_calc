c9943eddee32a48a792bf8e43fc3501a3279f108
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-07-31 17:23:41 -0400

    adding calc class

--- src/basic_Interest_Calc/interestCalculator.java
+++ src/basic_Interest_Calc/interestCalculator.java
@@ -1,5 +1,11 @@
 package basic_Interest_Calc;
 
 public class interestCalculator {
+	String interest;
+	String principal;
 
+	interestCalculator(String principal, String interest) {
+		this.principal = principal;
+		this.interest = interest;
+	}
 }
